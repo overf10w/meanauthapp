9096bbf0850a3c1d2e70a88a9d5019d23710899f
pwnzr <pvvner@yandex.ru>
2017-04-19 23:39:20 +0300

    Add user registration (/users/register POST method)

--- models/user.js
+++ models/user.js
@@ -1,36 +1,44 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const config = require('../config/database'); // 2:22
+const config = require('../config/database');
 
 // User Schema
 const UserSchema = mongoose.Schema({
-    // doesn't have to be required
-    name: { 
-        type: String 
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  // doesn't have to be required
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports.getUserByID = function(id, callback) {
-    User.findById(id, callback);
+module.exports.getUserByID = function (id, callback) {
+  User.findById(id, callback);
 };
 
-module.exports.getUserByUsername = function(id, callback) {
-    const query = { username: username };
-    User.findOne(query, callback);
+module.exports.getUserByUsername = function (id, callback) {
+  const query = { username: username };
+  User.findOne(query, callback);
 };
 
-
+module.exports.addUser = function(newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // Register
 router.post('/register', (req, res, next) => {
-    res.send('REGISTER');
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+  
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({ success: false, msg: 'Fail to register user' });
+    } else {
+      res.json({ success: true, msg: 'User registered' });
+    }
+  });
 });
 
 // Authenticate
 router.post('/authenticate', (req, res, next) => {
-    res.send('AUTHENTICATE');
+  res.send('AUTHENTICATE');
 });
 
 // Profile
 router.get('/profile', (req, res, next) => {
-    res.send('PROFILE');
+  res.send('PROFILE');
 });
 
 module.exports = router;
