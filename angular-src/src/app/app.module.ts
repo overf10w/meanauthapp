96a38eafb1dc3cc8677471ee87ff7f38f2d72821
pwnzr <pvvner@yandex.ru>
2017-04-20 16:27:48 +0300

    Add dashboard: in src/app/components/: ng g component dashboard

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HomeComponent } from './components/home/home.component';
     NavbarComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
