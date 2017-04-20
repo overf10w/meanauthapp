b97b04650ec9c83075458014bb0823a8a0cfd56d
pwnzr <pvvner@yandex.ru>
2017-04-20 19:51:58 +0300

    add validation to /register page (create ValidateService for this purpose)

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit() {
   }
@@ -22,6 +23,18 @@ export class RegisterComponent implements OnInit {
       username: this.username,
       email: this.email,
       password: this.password
+    };
+    
+    // Required Fields
+    if(!this.validateService.validateRegister(user)) {
+      console.log('Please fill in all fields');
+      return false;
+    }
+    
+    // Validate email
+    if(!this.validateService.validateEmail(user.email)) {
+      console.log('Please use a valid email');
+      return false;
     }
   }
 }
