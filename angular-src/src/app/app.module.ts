71baecfec13e5e156a73d09c3acb6a0b98832df3
pwnzr <pvvner@yandex.ru>
2017-04-20 20:29:22 +0300

    Add flashMessageService dependency (for showing flash messages)

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -12,7 +12,8 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
-import { ValidateService } from './services/validate.service'
+import { ValidateService } from './services/validate.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -36,7 +37,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
   providers: [ ValidateService ],
   bootstrap: [ AppComponent ]
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,8 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService) { }
+  constructor(private validateService: ValidateService,
+              private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
   }
@@ -24,15 +26,15 @@ export class RegisterComponent implements OnInit {
       email: this.email,
       password: this.password
     };
-    
+
     // Required Fields
-    if(!this.validateService.validateRegister(user)) {
+    if (!this.validateService.validateRegister(user)) {
       console.log('Please fill in all fields');
       return false;
     }
-    
+
     // Validate email
-    if(!this.validateService.validateEmail(user.email)) {
+    if (!this.validateService.validateEmail(user.email)) {
       console.log('Please use a valid email');
       return false;
     }
